c7e9d4754ee9b79b669adbee05b3c58ad76ba9ca
Nathan OGER <nathiii045@gmail.com>
2025-03-05 17:02:35 +0000

    /login revisité

--- symfony/src/Controller/AuthController.php
+++ symfony/src/Controller/AuthController.php
@@ -18,12 +18,13 @@ use Doctrine\DBAL\Exception\UniqueConstraintViolationException;
 
 class AuthController extends AbstractController
 {
-
     #[Route('/login', name: 'api_login', methods: ['POST'])]
     public function login(): JsonResponse
     {
-        // Cette méthode ne sera jamais exécutée car le firewall JWT intercepte la requête
-        throw new \LogicException('Cette méthode peut être vide - elle ne sera jamais exécutée !');
+        // Cette méthode ne sera quasiment jamais exécutée car le firewall JWT intercepte la requête (voir config/packages/security.yaml)
+        return $this->json([
+            'message' => "Désolé, l'authentification n'est pas disponible pour le moment.",
+        ]);
     }
 
     #[Route('/register', name: 'api_register', methods: ['POST'])]
