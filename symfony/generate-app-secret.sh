0098ebfa47cc8bfd0d0525c25722d43a52aca842
Nicolas-Delahaie <nico601.delahaie@gmail.com>
2025-04-06 16:32:36 +0200

    FIX du Dockerfile qui n'avait plus accès aux variables d'environnement + Factorisation du code uniquement pour a préparation

--- symfony/generate-app-secret.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Adds APP_SECRET only if it is missing
-if ! grep -qsE '^APP_SECRET=.*[^[:space:]]+' .env.local; then
-    SECRET=$(php -r 'echo bin2hex(random_bytes(16));')
-    echo "APP_SECRET=$SECRET" >> .env.local
-    echo "APP_SECRET created in .env.local : $SECRET"
-else
-    echo "APP_SECRET already exists in .env.local."
-fi
