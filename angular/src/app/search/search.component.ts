8b6a4fc95803339aba704f618d437f5502901436
jobmounchili <jobmounchili97@gmail.com>
2025-05-07 14:45:00 +0200

    Create card component

---
+++ angular/src/app/search/search.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { MatDatepickerModule} from '@angular/material/datepicker'
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInput, MatInputModule } from '@angular/material/input';
+import { MatLabel } from '@angular/material/form-field';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-search',
+  standalone: true,
+  imports: [MatFormFieldModule, MatInputModule, MatInput, MatLabel, MatToolbarModule, MatButtonModule, MatDatepickerModule],
+  templateUrl: './search.component.html',
+  styleUrl: './search.component.scss'
+})
+export class SearchComponent {
+
+}
