e04e93c8a2916837908a28bffac77f4c7e06772f
jobmounchili <jobmounchili97@gmail.com>
2025-05-27 10:35:53 +0200

    Update search component

--- angular/src/app/search/search.component.ts
+++ angular/src/app/search/search.component.ts
@@ -8,10 +8,12 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
+import { provideNativeDateAdapter } from '@angular/material/core';
 
 @Component({
   selector: 'app-search',
   standalone: true,
+  providers: [provideNativeDateAdapter()],
   imports: [MatFormFieldModule, MatInputModule, MatInput, MatLabel, MatToolbarModule, MatButtonModule, MatDatepickerModule],
   templateUrl: './search.component.html',
   styleUrl: './search.component.scss'
@@ -19,3 +21,4 @@ import { MatIconModule } from '@angular/material/icon';
 export class SearchComponent {
 
 }
+
