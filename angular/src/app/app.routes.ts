cbcdf46b68d25450c5b19982b19ae421a2e67a55
jobmounchili <jobmounchili97@gmail.com>
2025-05-28 13:04:09 +0200

    Save changes before merge

--- angular/src/app/app.routes.ts
+++ angular/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
+import { TravelDetailComponent } from './travel-detail/travel-detail.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'home', component: HomeComponent },
+  { path: 'travel-detail', component: TravelDetailComponent},
   { path: '', redirectTo: 'home', pathMatch: 'full' }
 ];
